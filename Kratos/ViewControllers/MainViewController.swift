e503784d60b9023976e145dbf60cc642cb46aba2
Dylanvs19 <dylan.v.straughan@gmail.com>
2016-08-16 00:59:51 -0400

    changing cards to tableView

--- Kratos/ViewControllers/MainViewController.swift
+++ Kratos/ViewControllers/MainViewController.swift
@@ -9,40 +9,16 @@
 
 import UIKit
 
-class MainViewController: UIViewController, RepViewDelegate {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, RepViewDelegate {
     
-    @IBOutlet var oneRepView: RepresentativeView!
-    @IBOutlet var twoRepView: RepresentativeView!
-    @IBOutlet var threeRepView: RepresentativeView!
-    
-    @IBOutlet var oneRepViewTopPosition: NSLayoutConstraint!
-    @IBOutlet var oneRepViewMiddlePosition: NSLayoutConstraint!
-    @IBOutlet var oneRepViewBottomPosition: NSLayoutConstraint!
-    
-    @IBOutlet var oneRepViewCompact: NSLayoutConstraint!
-    @IBOutlet var oneRepViewExpanded: NSLayoutConstraint!
-    
-    @IBOutlet var twoRepViewTopPosition: NSLayoutConstraint!
-    @IBOutlet var twoRepViewMiddlePosition: NSLayoutConstraint!
-    @IBOutlet var twoRepViewBottomPosition: NSLayoutConstraint!
-    
-    @IBOutlet var twoRepViewCompact: NSLayoutConstraint!
-    @IBOutlet var twoRepViewExpanded: NSLayoutConstraint!
-    
-    @IBOutlet var threeRepViewTopPosition: NSLayoutConstraint!
-    @IBOutlet var threeRepViewMiddlePosition: NSLayoutConstraint!
-    @IBOutlet var threeRepViewBottomPosition: NSLayoutConstraint!
-    
-    @IBOutlet var threeRepViewCompact: NSLayoutConstraint!
-    @IBOutlet var threeRepViewExpanded: NSLayoutConstraint!
-    
+    @IBOutlet var tableView: UITableView!
     var representatives: [Representative] = []
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         loadData()
-        setupRepresentatives()
     }
     
     func loadData() {
@@ -51,99 +27,28 @@ class MainViewController: UIViewController, RepViewDelegate {
         }
     }
     
-    func setupRepresentatives() {
-        let repViews = [(oneRepView, 0), (twoRepView, 1), (threeRepView, 2)]
-        repViews.map { (repView) in
-            repView.0.configure(with: representatives[repView.1])
-            setupRepViews(repView.0)
-            repView.0.repViewDelegate = self
-            switch repView.1 {
-            case 0:
-                repView.0.position = .top
-            case 1:
-                repView.0.position = .middle
-            default:
-                repView.0.position = .bottom
-            }
-        }
-    }
-    
-    func setupRepViews(repView: RepresentativeView) {
-        repView.representativeViewContentView.layer.cornerRadius = 3.0
-        repView.backgroundColor = UIColor(white: 0.5, alpha: 0.5)
-        repView.layer.shadowColor = UIColor.blackColor().CGColor
-        repView.layer.shadowOffset = CGSize(width: 0, height: 3)
-        repView.layer.shadowOpacity = 0.2
-        repView.layer.shadowRadius = 3
-    }
-    
-    func tapped(position: RepresentativeView.Position) {
-        if oneRepView.position == position {
-            
-        }
-        if twoRepView.position == position {
-            
-        }
-        if threeRepView.position == position {
-            
-        }
-    }
-    
-    func oneRepViewExpanded(bool: Bool) {
-        if bool {
-        UIView.animateWithDuration(1) {
-            self.oneRepViewBottomPosition.active = false
-            self.oneRepViewTopPosition.active = false
-            self.oneRepViewMiddlePosition.active = true
-            self.oneRepViewCompact.active = false
-            self.oneRepViewExpanded.active = true
-            self.view.layoutIfNeeded()
-        }
-        } else {
-            UIView.animateWithDuration(1) {
-                self.oneRepViewBottomPosition.active = false
-                self.oneRepViewMiddlePosition.active = false
-                self.oneRepViewTopPosition.active = true
-                self.oneRepViewExpanded.active = false
-                self.oneRepViewCompact.active = true
-                self.view.layoutIfNeeded()
-            }
- 
-        }
-    }
-    
-//    func oneRepViewShouldExitScreen(bool: Bool, withTappedScreen position: RepresentativeView.Position) {
-//        switch position {
-//        case .top:
-//            
-//        case .middle:
-//            <#code#>
-//        case .bottom:
-//            <#code#>
-//        }
-//        
-//        UIView.animateWithDuration(1) {
-//                self.view.layoutIfNeeded()
-//        }
-//    }
-//    
-    
     // MARK: RepViewDelegate Methods
-    func shouldExpand(bool: Bool, for position: RepresentativeView.Position) {
-        switch position {
-        case .top:
-            oneRepViewExpanded(bool)
-        case .middle:
-            break
-        case .bottom:
-            break
-        }
+    func shouldExpand(shouldExpand: Bool, for position: RepresentativeView.Position) {
+            switch position {
+            case .top:
+                break
+            case .middle:
+                break
+            case .bottom:
+                break
+            }
     }
     
     func panGestureYPosition(float: CGFloat, for position: RepresentativeView.Position) {
         
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return representatives.count
+    }
     
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
---
+++ Kratos/Views/TableViewCells/RepresentativeTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RepresentativeTableViewCell.swift
+//  Kratos
+//
+//  Created by Dylan Straughan on 8/16/16.
+//  Copyright © 2016 Dylan Straughan. All rights reserved.
+//
+
+import UIKit
+
+class RepresentativeTableViewCell: UITableViewCell {
+
+}
