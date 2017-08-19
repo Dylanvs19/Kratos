461cf4b756f0f390b9e3aec6edfffd466b8cd0f3
Dylanvs19 <dylan.v.straughan@gmail.com>
2017-08-19 15:54:28 -0400

    [UserController] cleaned up & rename userController

---
+++ Kratos/Source/Flows/User/UserController.swift
@@ -0,0 +1,66 @@
+//
+//  UserBillsController.swift
+//  Kratos
+//
+//  Created by Dylan Straughan on 8/1/17.
+//  Copyright © 2017 Dylan Straughan. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+import RxDataSources
+import SnapKit
+
+class UserController: UIViewController {
+    
+    // MARK: - Variables - 
+    // Standard
+    let client: Client
+    let viewModel: UserViewModel
+    let disposeBag = DisposeBag()
+    
+    // UIElements
+    let tableView = UITableView()
+    let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, Bill>>()
+
+    // MARK: - Initializers -
+    init(client: Client) {
+        self.client = client
+        self.viewModel = UserViewModel(client: client)
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        edgesForExtendedLayout = [.top, .right, .left]
+        addSubviews()
+        constrainViews()
+        styleViews()
+    }
+}
+
+    // MARK: - View Builder -
+extension UserController: ViewBuilder {
+    func addSubviews() {
+        view.addSubview(tableView)
+    }
+    
+    func constrainViews() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(60)
+            make.leading.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview().offset(-10)
+            make.bottom.equalToSuperview().offset(-5)
+        }
+    }
+    
+    func styleViews() {
+        view.style(with: .backgroundColor(.slate))
+    }
+}
---
+++ Kratos/Source/Flows/User/UserViewModel.swift
@@ -0,0 +1,60 @@
+//
+//  UserBillsViewModel.swift
+//  Kratos
+//
+//  Created by Dylan Straughan on 8/1/17.
+//  Copyright © 2017 Dylan Straughan. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+
+class UserViewModel {
+    
+    let client: Client
+    let disposeBag = DisposeBag()
+    let loadStatus = Variable<LoadStatus>(.none)
+    
+    let user = Variable<User?>(nil)
+    
+    let repSelected = PublishSubject<Person>()
+    
+    init(client: Client) {
+        self.client = client
+        bind()
+    }
+    
+    func fetchUser() -> Observable<User> {
+        loadStatus.value = .loading
+        return client.fetchUser()
+            .do(onNext: { [unowned self] user in
+                self.loadStatus.value = .none
+                }, onError: { [unowned self] (error) in
+                    let error = error as? KratosError ?? KratosError.unknown
+                    self.loadStatus.value = .error(error: error)
+            })
+    }
+    
+    func fetchRepresentatives(from state: String, district: Int) -> Observable<[Person]> {
+        loadStatus.value = .loading
+        return client.fetchRepresentatives(state: state, district: district)
+            .do(onNext: { [unowned self] _ in
+                self.loadStatus.value = .none
+                }, onError: { [unowned self] (error) in
+                    let error = error as? KratosError ?? KratosError.unknown
+                    self.loadStatus.value = .error(error: error)
+            })
+    }
+}
+
+extension UserViewModel: RxBinder {
+    
+    func bind() {
+        fetchUser().asObservable()
+            .bind(to: user)
+            .disposed(by: disposeBag)
+        
+    }
+}
