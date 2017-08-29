aa70ecf965ab8ea62cbd0f7c7926e55aec9870f4
Dylanvs19 <dylan.v.straughan@gmail.com>
2017-08-29 09:47:42 -0400

    [Erro] convenience function

--- Kratos/Source/Utilites/Extensions/UIFont+Extensions.swift
+++ Kratos/Source/Utilites/Extensions/UIFont+Extensions.swift
@@ -12,27 +12,27 @@ import UIKit
 extension UIFont {
     
     static var headerFont: UIFont {
-        return UIFont(name: "Futura", size: 24)!
+        return UIFont(name: "Futura", size: 24) ?? .systemFont(ofSize: 24)
     }
     static var titleFont: UIFont {
-        return UIFont(name: "Futura", size: 17)!
+        return UIFont(name: "Futura", size: 20) ?? .systemFont(ofSize: 20)
     }
     static var subHeaderFont: UIFont {
-        return UIFont(name: "Futura", size: 15)!
+        return UIFont(name: "Futura", size: 17) ?? .systemFont(ofSize: 17)
     }
     static var subTitleFont: UIFont {
-        return UIFont(name: "AvenirNext-Medium", size: 15)!
+        return UIFont(name: "AvenirNext-Medium", size: 17) ?? .systemFont(ofSize: 17)
     }
     static var cellTitleFont: UIFont {
-        return UIFont(name: "Futura", size: 14)!
+        return UIFont(name: "Futura", size: 15) ?? .systemFont(ofSize: 15)
     }
     static var cellSubtitleFont: UIFont {
-        return UIFont(name: "AvenirNext-Regular", size: 13)!
+        return UIFont(name: "AvenirNext-Regular", size: 14) ?? .systemFont(ofSize: 14)
     }
     static var bodyFont: UIFont {
-        return UIFont(name: "AvenirNext-Regular", size: 14)!
+        return UIFont(name: "AvenirNext-Regular", size: 15) ?? .systemFont(ofSize: 15)
     }
     static var textFont: UIFont {
-        return UIFont(name: "AvenirNext-Medium", size: 12)!
+        return UIFont(name: "AvenirNext-Medium", size: 13) ?? .systemFont(ofSize: 13)
     }
 }
